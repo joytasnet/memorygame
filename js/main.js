240da3b5884ebbb270603f2f66d7f703da5207e1
mjpurin <mjpurin@gmail>
2024-11-16 20:49:05 +0900

    Step 4: カード繰り返し作成されないようにする

--- js/main.js
+++ js/main.js
@@ -3,29 +3,30 @@ document.addEventListener('DOMContentLoaded', () => {
   // Cardクラス作成
   class Card {
     constructor(suit, num) {
-      this.suit = suit; // スート (s:スペード, d:ダイヤ...)
-      this.num = num;   // 数字 (1, 2, ... 13)
+      this.suit = suit;
+      this.num = num;
     }
   }
 
   // カード配列作成
   const cards = [];
-  const suits = ['s', 'd', 'h', 'c']; // スートの種類 (スペード, ダイヤ, ハート, クローバー)
+  const suits = ['s', 'd', 'h', 'c'];
 
   // 52枚のカードを作成
   suits.forEach(suit => {
     for (let num = 1; num <= 13; num++) {
-      cards.push(new Card(suit, num)); // 各カードを生成して配列に追加
+      cards.push(new Card(suit, num));
     }
   });
   // カードグリッドを初期化
   const cardgrid = document.getElementById('cardgrid');
   const initgrid = () => {
+    cardgrid.textContent=null;//cardgridに入っている要素をすべて削除
     cards.forEach(card => {
-      const div = document.createElement('div'); //div要素作成
-      div.textContent=card.suit+':'+card.num;//divのtextContentを設定
-      div.classList.add('card'); //divにcardクラスを追加
-      cardgrid.appendChild(div); // グリッドにカードを追加
+      const div = document.createElement('div');
+      div.textContent=card.suit+':'+card.num;
+      div.classList.add('card');
+      cardgrid.appendChild(div);
     });
   };
 
