a33c645cb4eea3fb43fcede273934976bb5798c0
mjpurin <mjpurin@gmail>
2024-11-16 20:56:16 +0900

    Step 9: 判定処理実装

--- js/main.js
+++ js/main.js
@@ -19,12 +19,39 @@ document.addEventListener('DOMContentLoaded', () => {
       cards.push(new Card(suit, num));
     }
   });
+  let firstCard=null;//1枚目のカードを保持(引いてない場合はnull)
+  let secondCard=null;//2枚目のカードを保持(引いてない場合はnull)
   //クリックした際の関数を定義
   const flip=(eve)=>{
-    //クリックされた要素を特定
     const div=eve.target;
-    //toggle(ついていたら外れ、外れていたら付く)
-    div.classList.toggle('back');
+    //div.classList.toggle('back');//この処理は不要なので削除
+    //表面のカードをクリックした場合や、3枚目のカードをクリックした場合はなにもしない
+    if(!div.classList.contains('back') || secondCard !== null){
+      return;
+    }
+    div.classList.remove('back');//表面にする
+    //もしそれが1枚目だったらfirstCardに代入
+    if(firstCard === null){
+      firstCard=div;
+    }else{
+      //2枚目だったらsecondCardに代入
+      secondCard=div;
+      //２枚のカードの数字が同じだったら
+      if(firstCard.num === secondCard.num){
+        //正解だった場合fadeoutクラスを付与する
+        firstCard.classList.add('fadeout');
+        secondCard.classList.add('fadeout');
+        //firstCard,secondカードを共にnullに戻す
+        [firstCard,secondCard]=[null,null];
+      }else{
+        //不正回だった場合は1.2秒後に裏面に戻す
+        setTimeout(()=>{
+          firstCard.classList.add('back');
+          secondCard.classList.add('back');
+          [firstCard,secondCard]=[null,null];
+        },1200);
+      }
+    }
   };
   // カードグリッドを初期化
   const cardgrid = document.getElementById('cardgrid');
@@ -35,6 +62,7 @@ document.addEventListener('DOMContentLoaded', () => {
       div.style.backgroundImage=`url(images/${card.front})`;
       div.classList.add('card','back');
       div.onclick=flip;
+      div.num=card.num;//divにnumプロパティを定義して、そこに数字を保存
       cardgrid.appendChild(div);
     });
   };
